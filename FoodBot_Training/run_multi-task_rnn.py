33d5634635cb6f210fe986c9551004a74166eb37
Jacky/Xingzhi Guo <guoxingzhi@gmail.com>
2017-05-15 13:59:11 +0800

    no message

--- FoodBot_Training/run_multi-task_rnn.py
+++ FoodBot_Training/run_multi-task_rnn.py
@@ -42,7 +42,7 @@ tf.app.flags.DEFINE_integer("max_train_data_size", 0,
                             "Limit on the size of training data (0: no limit).")
 tf.app.flags.DEFINE_integer("steps_per_checkpoint", 300,
                             "How many training steps to do per checkpoint.")
-tf.app.flags.DEFINE_integer("max_training_steps", 10000,
+tf.app.flags.DEFINE_integer("max_training_steps", 40000,
                             "Max training steps.")
 tf.app.flags.DEFINE_integer("max_test_data_size", 0,
                             "Max size of test set.")
