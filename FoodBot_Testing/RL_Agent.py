7fb2c2569fdbf4845718e44de68c47f4777061cd
Jacky/Xingzhi Guo <guoxingzhi@gmail.com>
2017-06-03 00:10:57 +0800

    no message

--- FoodBot_Testing/RL_Agent.py
+++ FoodBot_Testing/RL_Agent.py
@@ -194,6 +194,7 @@ def hasNewTurn(formerAction,formerReward,currentState,d,formerState):
     s = formerState
     a = formerAction
     r = formerReward
+    r = 1
     s1 = currentState
     d = False # The termination indiction 
     #In our case, the termiantion states are: [0,0,0,0,0,0,0,0,0,0,0],[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
