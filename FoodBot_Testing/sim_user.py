957945ae0caa45e9ebc5cfcd3278332ffcbb558b
Jacky/Xingzhi Guo <guoxingzhi@gmail.com>
2017-06-03 10:44:39 +0800

    no message

--- FoodBot_Testing/sim_user.py
+++ FoodBot_Testing/sim_user.py
@@ -103,7 +103,7 @@ def policyChecker(sys_act):
 		if sys_act['intent'] == 'inform':
 			return 3
 	elif memory['intent'] == 'no':
-		if sys_act == ''
+		if sys_act == '':
 			return 3
 	elif expect == 'get_restaurant':
 		if sys_act['intent'] == 'request':
