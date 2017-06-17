9e2295ab6bc2ffb93088cc5f70a302185aba85c9
hrbq <zeroblakejack@gmail.com>
2017-06-18 02:02:04 +0800

    change css style

--- FoodBot_GRPC_Client/www/scripts/foodbot3.js
+++ FoodBot_GRPC_Client/www/scripts/foodbot3.js
@@ -43,14 +43,28 @@ FoodBot.prototype = {
 	
 	_displayNewMsg: function(name,msg,color){
 		var container = document.getElementById('historyMsg'),
-			msgToDisplay = document.createElement('p'),
+			msgToDisplay = document.createElement('div'),
 			date = new Date().toTimeString().substr(0,8);
-		msgToDisplay.style.color = color || '#000';
+		//msgToDisplay.style.color = color || '#000';
+		msgToDisplay.style.color = 'white';
+		msgToDisplay.style.background = '#0084ff';
+		msgToDisplay.style.border = '2px #ccc solid';
+		msgToDisplay.style.marginTop = '1px';
+		msgToDisplay.style.marginBottom = '0.5em';
+		msgToDisplay.style.borderRadius = '1.3em'
+		msgToDisplay.style.paddingTop = '6px'
+		msgToDisplay.style.paddingBottom = '6px'
+		msgToDisplay.style.paddingLeft = '12px'
+		msgToDisplay.style.paddingRight = '12px'
 		if(name != 'FoodBot'){
+			msgToDisplay.style.float = 'right';
 			msgToDisplay.style.textAlign = 'right';
-			msgToDisplay.innerHTML = '<span class="timespan">(' + date + ')</span>' + msg + ': ' + user ;
+			//msgToDisplay.innerHTML = '<span class="timespan">(' + date + ')</span>' + msg + ': ' + user ;
+			msgToDisplay.innerHTML = msg + ': ' + user ;
 		}else{
-			msgToDisplay.innerHTML = name + ': ' + msg + '<span class="timespan">(' + date + ')</span>';	
+			msgToDisplay.style.float = 'left';
+			//msgToDisplay.innerHTML = name + ': ' + msg + '<span class="timespan">(' + date + ')</span>';
+			msgToDisplay.innerHTML = name + ': ' + msg;	
 		}
 		container.appendChild(msgToDisplay);
 		container.scrollTop = container.scrollHeight;
