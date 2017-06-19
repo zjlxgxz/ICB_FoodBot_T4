61ee2bfe248bbc35e5322f8320e38f3714bb5b18
hrbq <zeroblakejack@gmail.com>
2017-06-19 21:29:39 +0800

    add update client side code

--- FoodBot_GRPC_Client/www/scripts/foodbot3.js
+++ FoodBot_GRPC_Client/www/scripts/foodbot3.js
@@ -1,10 +1,11 @@
-var user;
+var user, mute, showMEME, mode, queryMsg;
 
 window.onload = function () {
 	var foodbot = new FoodBot();
 	popupUserName();
 	foodbot.init();
-
+	showMEME = false;
+	mode = 'goToGRPC';
 };
 
 var FoodBot = function () {
@@ -16,10 +17,18 @@ FoodBot.prototype = {
 		var that = this;
 		this.socket = io.connect();
 		this.socket.on('connect', function() {
+			that._displayNewMsg('FoodBot','Hi what can I do for you:)');
+			speak('Hi what can I do for you');
 		});
-		this.socket.on('newMsg', function(name, msg, color){
-			if(msg != null){
-				that._displayNewMsg(name, msg, color);
+		this.socket.on('newMsg', function(name, msg, url){
+			if(msg != null && isNormalUrl(url)){
+				that._displayNewMsg(name, msg, url);
+			}else{
+				mode = 'goToDB';
+				queryMsg = JSON.parse(url);
+				that.displayTableMsg(name, msg);
+			}
+			if (!mute) {
 				speak(msg);
 			}
 		});
@@ -35,41 +44,56 @@ FoodBot.prototype = {
 			messageInput.value = '';
 			messageInput.focus();
 			if(msg != null){
-				that.socket.emit('postMsg', msg, user);
-				that._displayNewMsg(user,msg, 'green');
+				if(mode == 'goToGRPC'){
+					that.socket.emit('postMsg', msg, user);
+				}else{
+					that.socket.emit('postMsgToDB', modifyQueryContent(msg));
+					mode = 'goToGRPC';
+				}
+				that._displayNewMsg(user,msg, url);
 			};
 		},false);
+
+		document.getElementById('mute').addEventListener('change',function(){
+			var muteckBox = document.getElementById('mute');
+			if(muteckBox.checked){
+				mute = true;
+			}else{
+				mute = false;
+			}
+		});
+
+		document.getElementById('talkStyle').addEventListener('change',function(){
+			var selector = document.getElementById('talkStyle');
+			if(selector.value == 'normal'){
+				showMEME = false;
+			}else{
+				showMEME = true;
+			}
+		});
 	},
 	
-	_displayNewMsg: function(name,msg,color){
+	_displayNewMsg: function(name,msg,url){
 		var container = document.getElementById('historyMsg'),
-			msgToDisplay = document.createElement('div'),
-			date = new Date().toTimeString().substr(0,8);
-		//msgToDisplay.style.color = color || '#000';
-		msgToDisplay.style.color = 'white';
-		msgToDisplay.style.background = '#0084ff';
-		msgToDisplay.style.border = '2px #ccc solid';
-		msgToDisplay.style.marginTop = '1px';
-		msgToDisplay.style.marginBottom = '0.5em';
-		msgToDisplay.style.borderRadius = '1.3em'
-		msgToDisplay.style.paddingTop = '6px'
-		msgToDisplay.style.paddingBottom = '6px'
-		msgToDisplay.style.paddingLeft = '12px'
-		msgToDisplay.style.paddingRight = '12px'
+			msgToDisplay = document.createElement('div');
 		if(name != 'FoodBot'){
+			msgToDisplay.style.color = 'white';
+			msgToDisplay.style.background = '#0084ff';
 			msgToDisplay.style.float = 'right';
 			msgToDisplay.style.textAlign = 'right';
-			//msgToDisplay.innerHTML = '<span class="timespan">(' + date + ')</span>' + msg + ': ' + user ;
 			msgToDisplay.innerHTML = msg + ': ' + user ;
 		}else{
+			msgToDisplay.style.color = 'black';
+			msgToDisplay.style.background = '#f1f0f0';
 			msgToDisplay.style.float = 'left';
-			//msgToDisplay.innerHTML = name + ': ' + msg + '<span class="timespan">(' + date + ')</span>';
 			msgToDisplay.innerHTML = name + ': ' + msg;	
 		}
 		container.appendChild(msgToDisplay);
 		container.scrollTop = container.scrollHeight;
+		if(name == 'FoodBot' && showMEME && url){
+			displayNewMEME(name, url);
+		}
 	}
-
 };
 
 function popupUserName(){
@@ -78,4 +102,74 @@ function popupUserName(){
 		userName = prompt("讓我們重來一次，君の名は？")
 	}
 	user = userName;
+};
+
+// TODO show table message
+function displayTableMsg(name, msg, url){
+	var container = document.getElementById('historyMsg'),
+		picToDisplay = document.createElement('div');
+	picToDisplay.style.background = '#f1f0f0';
+	picToDisplay.style.float = 'left';
+
+	var tableDetail = JSON.parse(url);
+	picToDisplay.appendChild(document.createTextNode(name + ': ' + msg));
+	var table = document.createElement("table");
+	for(var k in tableDetail){
+		var tr = document.createElement('tr');   
+		var td1 = document.createElement('td');
+		var td2 = document.createElement('td');
+		var text1 = document.createTextNode(k + ':');
+		var text2 = document.createTextNode(tableDetail[k]);
+
+		td1.appendChild(text1);
+		td2.appendChild(text2);
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+
+		table.appendChild(tr);
+	}
+	picToDisplay.appendChild(table);
+	container.appendChild(picToDisplay);
+
+	container.scrollTop = container.scrollHeight;
+};
+
+function displayNewMEME(name,url){
+	var container = document.getElementById('historyMsg'),
+		picToDisplay = document.createElement('div');
+	picToDisplay.style.background = '#f1f0f0';
+	picToDisplay.style.float = 'left';
+	picToDisplay.innerHTML = name + ": <img src="+ url+" style='height:80px;width:80px;'>";
+	container.appendChild(picToDisplay);
+	container.scrollTop = container.scrollHeight;
+};
+
+function modifyQueryContent(msg){
+	var msgArray = strToObjParser(msg);
+	Object.keys(queryMsg).forEach(function(key){
+		if(queryMsg[key]==null || queryMsg[key]==0){
+			queryMsg[key] = msgArray[key];
+		}
+	});
+	return queryMsg;
+};
+
+function strToObjParser(str) {
+    return str.split(',').map(function(el) {
+        return el.trim();
+    }).map(function(el) {
+        return el.split(':');
+    }).reduce(function(result, pair) {
+        result[pair[0].trim()] = pair[1].trim();
+        return result;
+    }, {});
+}
+
+function isNormalUrl(url){
+  if(url){
+    var urlStr = url.trim();
+    return urlStr.match(/(http){1}/g) == 'http'? true : false;
+  }else{
+    return true;
+  }
 };
