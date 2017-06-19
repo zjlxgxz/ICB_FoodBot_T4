c36c2a6bdfe398f186e478321655ca414932ca65
hrbq <zeroblakejack@gmail.com>
2017-06-20 02:56:23 +0800

    update client

--- FoodBot_GRPC_Client/www/scripts/foodbot3.js
+++ FoodBot_GRPC_Client/www/scripts/foodbot3.js
@@ -179,7 +179,7 @@ function isNormalUrl(url){
   }
 };
 
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -189,7 +189,7 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
